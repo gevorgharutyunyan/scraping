0b6b45c112400efd9f04d8ae4f1176f90ec45e95
Gevorg Harutyunyan <39996731+gevorgharutyunyan@users.noreply.github.com>
2021-01-11 15:52:00 +0400

    working version

--- ScrapeSourceData/ScrapeSourceData.py
+++ ScrapeSourceData/ScrapeSourceData.py
@@ -7,10 +7,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-"""source = "CTA 102"  # Source name for which other catalog names should be found
+source = "1ES 0033+595"  # Source name for which other catalog names should be found
 driver = webdriver.Chrome() # Used Google Chrome WEB browser
 driver.get("https://openuniverse.asi.it/open_universe.html") # This page is used for scraping
-
+driver.maximize_window()
 element = driver.find_element_by_id('stringa_inserita_su_homepage') #Firstly need to fill input tag of a web page with source name
 element.send_keys(source)    # Tag ID of an input tag is "stringa_inserita_su_homepage "
 time.sleep(10)    # Need a delay maybe connection is not good
@@ -24,14 +24,14 @@ try:
     html = titles.get_attribute('innerHTML') # Get inner text of tha span tag
     names = html.split("<br>") # Make a list using split function. <br> tag was used for new line in HTML
     del names[-1] # Last <br> tag replaced by "," and this should be deleted from the list
-    print(names)
+    #print(names)
 finally:
     driver.quit() # Anyway close the browser
-"""
 
 # Used Google Chrome WEB browser
 
-names =['PKSB2230+114', 'BZQJ2232+1143', 'PKS2230+114', 'WMAP47', '5BZQJ2232+1143', 'GB6B2230+1128', 'PLCKERC143G077.43-38.58', 'WMAP5J1254+1142', 'GB6J2232+1143', 'PLCKERC070G077.43-38.58', '1RXSJ223236.8+114331', '2FGLJ2232.4+1143', '1Jy2230+114', 'PCCS1030G077.44-38.58', '2E2230.1+1128', 'PLCKERC217G077.44-38.57', 'PCCS1044G077.44-38.59', '1FGL_J2232.5+1144', 'CTA102', 'PLCKERC353G077.43-38.58', 'PLCKERC044G077.43-38.58']
+#names =['PKSB2230+114', 'BZQJ2232+1143', 'PKS2230+114', 'WMAP47', '5BZQJ2232+1143', 'GB6B2230+1128', 'PLCKERC143G077.43-38.58', 'WMAP5J1254+1142', 'GB6J2232+1143', 'PLCKERC070G077.43-38.58', '1RXSJ223236.8+114331', '2FGLJ2232.4+1143', '1Jy2230+114', 'PCCS1030G077.44-38.58', '2E2230.1+1128', 'PLCKERC217G077.44-38.57', 'PCCS1044G077.44-38.59', '1FGL_J2232.5+1144', 'CTA102', 'PLCKERC353G077.43-38.58', 'PLCKERC044G077.43-38.58']
+#names =[ 'BZQJ2232+1143', 'PKS2230+114', 'WMAP47']
 
 #for i in names:
 #    matched_elements = browser.get("https://www.google.com/search?q=" + i)
@@ -39,20 +39,33 @@ names =['PKSB2230+114', 'BZQJ2232+1143', 'PKS2230+114', 'WMAP47', '5BZQJ2232+114
 
 browser = webdriver.Chrome()
 browser.get('http://www.google.com')
+browser.maximize_window()
+valid_link=[]
+for name in names:
+    search = browser.find_element_by_name('q')
+    search.send_keys(name)
+    search.send_keys(Keys.RETURN) # hit return after you enter search text
+    time.sleep(5) # sleep for 5 seconds so you can see the results
+    search_result = browser.find_elements_by_class_name("yuRUbf")
+    links = []
+    for i in search_result:
+        child = i.find_elements_by_tag_name("a")
+        for j in child:
+            links.append(j.get_attribute('href'))
+    browser.find_element_by_name('q').clear()
+    time.sleep(2)
+    valid_links=[k for k in links if  "https://translate.google.com/" not in k]
+    for link in valid_links:
+        valid_link.append(link)
+browser.quit()
+open_links = webdriver.Chrome()
+open_links.maximize_window()
+for info_link in range(len(valid_link)):
+    open_links.get(valid_link[info_link])
+    if (info_link != len(valid_link) - 1):
+        open_links.execute_script("window.open('');")
+        chwd = open_links.window_handles
+        open_links.switch_to.window(chwd[-1])
+    time.sleep(3)
 
-search = browser.find_element_by_name('q')
-search.send_keys("PKSB2230+114")
-search.send_keys(Keys.RETURN) # hit return after you enter search text
-time.sleep(5) # sleep for 5 seconds so you can see the results
-#browser.quit()
 
-"""links = browser.find_elements_by_class_name("//ol[@class='web_regular_results']//h3//a")
-results = []
-
-
-for link in links:
-    href = link.get_attribute("href")
-    print(href)
-    results.append(href)
-    browser.close()
-"""
