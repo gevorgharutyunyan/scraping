cfd58b0746cd4a8c2ee8a6a1aa0c1056cea160ad
Gevorg Harutyunyan <39996731+gevorgharutyunyan@users.noreply.github.com>
2021-01-14 17:34:51 +0400

    changed

--- ScrapeSourceData/ScrapeSourceData.py
+++ ScrapeSourceData/ScrapeSourceData.py
@@ -7,15 +7,16 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-source = "4C+13.14"  # Source name for which other catalog names should be found
+source = "PKS 0420+022"  # Source name for which other catalog names should be found
 driver = webdriver.Chrome() # Used Google Chrome WEB browser
 driver.get("https://openuniverse.asi.it/open_universe.html") # This page is used for scraping
 driver.maximize_window()
 element = driver.find_element_by_id('stringa_inserita_su_homepage') #Firstly need to fill input tag of a web page with source name
 element.send_keys(source)    # Tag ID of an input tag is "stringa_inserita_su_homepage "
-time.sleep(7)    # Need a delay maybe connection is not good
+time.sleep(10)    # Need a delay maybe connection is not good
 
-found_source = driver.find_element_by_id('page').click() # If source name is found click on a main page to see more about source
+#found_source = driver.find_element_by_id('page').click() # If source name is found click on a main page to see more about source
+found_source=element.send_keys(Keys.RETURN)
 time.sleep(5) # Wait 5 seconds
 
 try:
@@ -24,7 +25,8 @@ try:
     html = titles.get_attribute('innerHTML') # Get inner text of tha span tag
     names = html.split("<br>") # Make a list using split function. <br> tag was used for new line in HTML
     del names[-1] # Last <br> tag replaced by "," and this should be deleted from the list
-    #print(names)
+    print(names)
+    driver.delete_all_cookies()
 finally:
     driver.quit() # Anyway close the browser
 #names =[ 'BZQJ2232+1143', 'PKS2230+114', 'WMAP47']
@@ -33,31 +35,34 @@ browser = webdriver.Chrome()
 browser.get('http://www.google.com') # Search using all names in google.com
 browser.maximize_window() #maximize chrome window after opening
 clear_links=[] # gather all links in this list connected with source name
-for name in names:
-    search = browser.find_element_by_name('q') # find search box
-    search.send_keys(name) # fill search box with a source name
-    search.send_keys(Keys.RETURN) # hit return after you entering source name
-    time.sleep(5) # sleep for 5 seconds so you can see the results
-    search_result = browser.find_elements_by_class_name("yuRUbf") #Each result has a class with name yuRUbf
-    links = []
-    for i in search_result:
-        child = i.find_elements_by_tag_name("a") # separate <a> tags which contains searching result links
-        for j in child:
-            links.append(j.get_attribute('href')) #from <a> tag get only href
-    browser.find_element_by_name('q').clear() # after filling search box and finding results clear searched name
-    time.sleep(2)
-    valid_links=[k for k in links if  "https://translate.google.com/" not in k] # links list contain other links which need to be removed
-    for link in valid_links:
-        clear_links.append(link)
+if names:
+    for name in names:
+       search = browser.find_element_by_name('q') # find search box
+       search.send_keys(name) # fill search box with a source name
+       search.send_keys(Keys.RETURN) # hit return after you entering source name
+       time.sleep(5) # sleep for 5 seconds so you can see the results
+       search_result = browser.find_elements_by_class_name("yuRUbf") #Each result has a class with name yuRUbf
+       links = []
+       for i in search_result:
+           child = i.find_elements_by_tag_name("a") # separate <a> tags which contains searching result links
+           for j in child:
+               links.append(j.get_attribute('href')) #from <a> tag get only href
+       browser.find_element_by_name('q').clear() # after filling search box and finding results clear searched name
+       time.sleep(2)
+       valid_links=[k for k in links if  "https://translate.google.com/" not in k] # links list contain other links which need to be removed
+       if valid_links:
+           for link in valid_links:
+               clear_links.append(link)
 browser.quit() # after getting all results close the browser
-open_links = webdriver.Chrome() # then need to open browser again to open gathered links
-open_links.maximize_window()
-for info_link in range(len(clear_links)):
-    open_links.get(clear_links[info_link])
-    if (info_link != len(clear_links) - 1):
-        open_links.execute_script("window.open('');") # open tab for each new link from clear_links
-        chwd = open_links.window_handles
-        open_links.switch_to.window(chwd[-1])
-    time.sleep(3)
+if clear_links:
+    open_links = webdriver.Chrome() # then need to open browser again to open gathered links
+    open_links.maximize_window()
+    for info_link in range(len(clear_links)):
+        open_links.get(clear_links[info_link])
+        if (info_link != len(clear_links) - 1):
+            open_links.execute_script("window.open('');") # open a new tab for each new link from clear_links
+            chwd = open_links.window_handles
+            open_links.switch_to.window(chwd[-1])
+        time.sleep(3)
 
 
---
+++ ScrapeSourceData/nustar.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def find_nustar_obs(source):
+    driver = webdriver.Chrome() # Used Google Chrome WEB browser
+    driver.get("https://www.ssdc.asi.it/mmia/index.php?mission=numaster") # This page is used for scraping
+    driver.maximize_window()
+    element = driver.find_element_by_id('sourcein') #Firstly need to fill input tag of a web page with source name datacatalog
+    element.send_keys(source)
+    element.send_keys(Keys.RETURN)
+    driver.switch_to.window(driver.window_handles[1])
+    if driver.current_url =="https://www.ssdc.asi.it/cgi-bin/catalogs":
+
+        try:
+            driver.find_element_by_tag_name('img')
+        except:
+            return source
+        else:
+            return False
+    if "https://www.ssdc.asi.it/js/index.php?missione=numaster&file=" in driver.current_url:
+        return True
+    driver.quit()
+
+
+source_names = ['3C6.1', '3C9', '3C15', '3C17', 'NGC315', '3C31', '0106+013', '3C33', '3C47', '4C+35.03',
+                'PKS0208-512', '3C66B', '0234+285', '0313-192', '3C83.1', 'PKS0405-12', '3C109', 'PKS0413-21',
+                '3C111', '3C120', '3C123', '3C129', '0454-463', 'PictorA', 'PKS0521-36', '0529+075', 'PKS0605-08',
+                'PKS0637-752', '3C173.1', '3C179', 'B20738+313', 'DA240', '3C189', '0827+243', '4C+29.30', '3C207',
+                'OJ287', '3C212', 'PKS0903-573', '6C0905+39', 'PKS0920-397', '3C219', '3C227', 'Q0957+561',
+                '4C+13.41', 'PKS1030-357', '1045-188', 'PKS1046-409', 'PKS1055+201', '3C254', 'PKS1127-145',
+                'PKS1136-135', '3C263', '3C264', '3C265', '4C+49.22', 'PKS1202-262', 'NGC4261', '1222+216', 'M84',
+                '3C273', 'M87', 'PKS1229-02', '3C275.1', '3C280', '3C281', '1317+520', '4C+65.15', 'CenA',
+                '1334-127', 'CenB', '4C+19.44', '3C294', '3C295', '3C296', 'PKS1421-490', '3C303', '1508+572',
+                'PKS1510-08', 'AP Lib', '3C321', '3C324', '4C+00.58', '3C327', '3C330', 'NGC6251', '3C345', '3C346',
+                '1642+690', '3C351', '3C353', '1745+624', '1800+440', '3C371', '3C380', '3C390.3', '1849+670',
+                '1928+738', '3C403', 'CygA', 'S52007+777', '4C+74.26', 'PKS2101-49', '2123-463', 'PKS2152-69',
+                '2155-152', '2201+315', 'PKS2201+044', '2209+080', '2216-038', '3C445', '3C452', '3C454.3',
+                '2255-282', 'PKSJ2310-437', '3C465', '2345-167']
+
+
+obs_list = list(map(find_nustar_obs,source_names))
+
+print(obs_list)
